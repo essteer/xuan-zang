cc6847c1ea329f79dfa95d6cbdbc636a1465ddce
essteer <essteer@pm.me>
2023-04-12 19:09:30 +0800

    add top scroll button

---
+++ public/javascripts/toTopButton.js
@@ -0,0 +1,25 @@
+//Get the button
+let topButton = document.getElementById("btn-back-to-top");
+
+// When the user scrolls down 100px from the top of the document, show the button
+window.onscroll = function () {
+  scrollFunction();
+};
+
+function scrollFunction() {
+  if (
+    document.body.scrollTop > 100 ||
+    document.documentElement.scrollTop > 100
+  ) {
+    topButton.style.display = "block";
+  } else {
+    topButton.style.display = "none";
+  }
+}
+// When the user clicks on the button, scroll to the top of the document
+topButton.addEventListener("click", backToTop);
+
+function backToTop() {
+  document.body.scrollTop = 0;
+  document.documentElement.scrollTop = 0;
+}
