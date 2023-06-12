b2bd312bae4291d608a108763f8f56024e8a123b
essteer <essteer@pm.me>
2023-06-12 19:43:48 +0300

    change topButton variable from let to const

--- public/javascripts/toTopButton.js
+++ public/javascripts/toTopButton.js
@@ -1,5 +1,5 @@
 //Get the button
-let topButton = document.getElementById("btn-back-to-top");
+const topButton = document.getElementById("btn-back-to-top");
 
 // When the user scrolls down 100px from the top of the document, show the button
 window.onscroll = function () {
