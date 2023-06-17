076c0a5583bbdc6db9cb3f1df9cf76e54d730f1f
essteer <essteer@pm.me>
2023-06-17 14:15:00 +0300

    replace getElementById with querySelector for topscroll button

--- public/javascripts/toTopButton.js
+++ public/javascripts/toTopButton.js
@@ -1,5 +1,5 @@
 //Get the button
-const topButton = document.getElementById("btn-back-to-top");
+const topButton = document.querySelector("#btn-back-to-top");
 
 // When the user scrolls down 100px from the top of the document, show the button
 window.onscroll = function () {
