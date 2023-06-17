78656593c4933fd397229d69050dc18778e4079b
essteer <essteer@pm.me>
2023-06-17 14:40:12 +0300

    update browser support of toTopButton

--- public/javascripts/toTopButton.js
+++ public/javascripts/toTopButton.js
@@ -7,19 +7,27 @@ window.onscroll = function () {
 };
 
 function scrollFunction() {
-  if (
-    document.body.scrollTop > 100 ||
-    document.documentElement.scrollTop > 100
-  ) {
+  // Get the scroll position from different properties depending on browser support
+  const scrollPosition =
+    window.scrollY || // Modern browsers
+    document.documentElement.scrollTop || // Firefox and IE
+    document.body.scrollTop; // Legacy fallback for Safari
+
+  // Show or hide the button based on the scroll position
+  if (scrollPosition > 100) {
     topButton.style.display = "block";
   } else {
     topButton.style.display = "none";
   }
 }
+
 // When the user clicks on the button, scroll to the top of the document
 topButton.addEventListener("click", backToTop);
 
 function backToTop() {
-  document.body.scrollTop = 0;
-  document.documentElement.scrollTop = 0;
+  // Scroll to the top of the document with a smooth scrolling effect
+  document.documentElement.scrollTo({
+    top: 0,
+    behavior: "smooth",
+  });
 }
