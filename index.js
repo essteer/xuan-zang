8623455a1a871afbe8eeb25237046cefd5147518
essteer <essteer@pm.me>
2024-09-19 09:38:23 +0800

    fix: Ensure requested file exists within Markdown page directory as rendering condition, to prevent path traversal

--- index.js
+++ index.js
@@ -6,7 +6,7 @@ const express = require("express");
 const rateLimit = require("express-rate-limit");
 const app = express();
 // Disable X-Powered-By header for your Express app (Snyk)
-app.disable('x-powered-by');
+app.disable("x-powered-by");
 const path = require("path");
 const ejsMate = require("ejs-mate");
 // Built-in module to access and interact with file system
@@ -14,14 +14,12 @@ const fs = require("fs");
 // Parse front matter from Markdown files
 const { marked } = require("marked");
 
-
 app.engine("ejs", ejsMate);
 app.set("view engine", "ejs");
 // Configure path to "views" directory
 app.set("views", path.join(__dirname, "views"));
 // Configure path to "public" directory
 app.use(express.static(path.join(__dirname, "public")));
-
 
 // Apply rate limiting to all requests
 const limiter = rateLimit({
@@ -32,11 +30,14 @@ const limiter = rateLimit({
 });
 app.use(limiter);
 
-
 // Main index page
 app.get("/", (req, res) => {
   // sameSite: "lax" improves navigation performance by permitting back/forward cache restoration
-  res.cookie("cookieName", "cookieValue", { sameSite: "lax", secure: true, httpOnly: true });
+  res.cookie("cookieName", "cookieValue", {
+    sameSite: "lax",
+    secure: true,
+    httpOnly: true,
+  });
   res.render("index");
 });
 
@@ -54,18 +55,35 @@ app.get("/about", (req, res) => {
   res.render("about/about");
 });
 
+function isValidFilename(filename) {
+  return (
+    !filename.includes("..") &&
+    !filename.startsWith(".") &&
+    !filename.includes("/")
+  );
+}
+
+// Permitted Markdown pages exist in this directory
+const pagesDirectory = path.join(__dirname, "views", "misc", "pages");
 // Markdown to HTML rendering for entire pages
 app.get("/:filename", (req, res) => {
   const filename = req.params.filename;
-  const markdownPath = path.join(__dirname, "views", "misc", "pages", `${filename}.md`);
-  
-  fs.readFile(markdownPath, "utf8", (err, data) => {
+  const markdownPath = path.join(pagesDirectory, `${filename}.md`);
+
+  fs.access(markdownPath, fs.constants.F_OK, (err) => {
     if (err) {
       res.status(404);
       res.render("utils/pagenotfound");
     } else {
-      const htmlContent = marked(data);
-      res.render("misc/misc", { content: htmlContent });
+      fs.readFile(markdownPath, "utf8", (err, data) => {
+        if (err) {
+          res.status(404);
+          res.render("utils/pagenotfound");
+        } else {
+          const htmlContent = marked(data);
+          res.render("misc/misc", { content: htmlContent });
+        }
+      });
     }
   });
 });
